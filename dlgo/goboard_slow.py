a64944ddf7abc83a8898be6fa806b10a7eb6e88f
fuurin <komatsuemgbr0624@gmail.com>
2019-06-28 02:04:29 +0900

    p75

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -1,24 +1,26 @@
 import copy
 from .gotypes import Player
 
+
 class Move():
     """ターン，着手のクラス．打石，パス，投了のいずれか"""
+
     def __init__(self, point=None, is_pass=False, is_resign=False):
-        assert (point is not None) ^ is_pass ^ is_resign # どれかひとつしか受け付けないってこと
+        assert (point is not None) ^ is_pass ^ is_resign  # どれかひとつしか受け付けないってこと
         self.point = point
-        self.is_play = (self.point is not None) # 打石であるかどうか
+        self.is_play = (self.point is not None)  # 打石であるかどうか
         self.is_pass = is_pass
         self.is_resign = is_resign
-        
+
     # classmethod アノテーションでインスタンス化なしで利用可能なメソッドを指定
     @classmethod
     def play(cls, point):
         return Move(point=point)
-    
+
     @classmethod
     def pass_turn(cls):
         return Move(is_pass=True)
-    
+
     @classmethod
     def resign(cls):
         return Move(is_resign=True)
@@ -26,19 +28,20 @@ class Move():
 
 class GoString():
     """石の並び，連のクラス"""
+
     def __init__(self, color, stones, liberties):
         self.color = color
 
         # 連を構成する石，および呼吸点はsetとして保持する
         self.stones = set(stones)
         self.liberties = set(liberties)
-    
+
     def remove_liberty(self, point):
         self.liberties.remove(point)
-    
+
     def add_liberty(self, point):
         self.liberties.add(point)
-    
+
     # プレイヤーが石を置いて二つのグループを連結するときに呼び出される
     def merged_with(self, go_string):
         assert go_string.color == self.color
@@ -52,24 +55,25 @@ class GoString():
     @property
     def num_liberties(self):
         return len(self.liberties)
-    
+
     def __eq__(self, other):
         return isinstance(other, GoString) and \
             self.color == other.color and \
-            self.stones == other.stoners and \
+            self.stones == other.stones and \
             self.liberties == other.liberties
-    
+
 
 class Board():
     """石を置くためのルールと取るためのルール"""
+
     def __init__(self, num_rows, num_cols):
         self.num_rows = num_rows
         self.num_cols = num_cols
-        self._grid = {} # GoStringを格納する辞書
+        self._grid = {}  # GoStringを格納する辞書
 
     def place_stone(self, player, point):
-        assert self.is_on_grid(point) # 打石点が盤面の範囲内にある
-        assert self._grid.get(point) is None # まだその点には打たれていない
+        assert self.is_on_grid(point)  # 打石点が盤面の範囲内にある
+        assert self._grid.get(point) is None  # まだその点には打たれていない
         adjacent_same_color = []
         adjacent_opposite_color = []
         liberties = []
@@ -80,7 +84,7 @@ class Board():
             # 隣接点が盤面の範囲外ならcontinue
             if not self.is_on_grid(neighbor):
                 continue
-            
+
             # 隣接点にある連についてしらべる
             neighbor_string = self._grid.get(neighbor)
 
@@ -95,25 +99,25 @@ class Board():
                 else:
                     if neighbor_string not in adjacent_opposite_color:
                         adjacent_opposite_color.append(neighbor_string)
-            
-            new_string = GoString(player, [point], liberties)
 
-            # 周囲にある連を全てマージする
-            for same_color_string in adjacent_same_color:
-                new_string = new_string.merged_with(same_color_string)
-            
-            # マージした連の石一つ一つのポイントから，新しくできた連にアクセスできるようにする
-            for new_string_point in new_string.stones:
-                self._grid[new_string_point] = new_string
-            
-            # 隣接する相手の連から，この石の場所の呼吸点を取り除く
-            for other_color_string in adjacent_opposite_color:
-                other_color_string.remove_liberty(point)
-            
-            # 隣接する相手の連の呼吸点が0になったらその連を取り除く
-            for other_color_string in adjacent_opposite_color:
-                if other_color_string.num_liberties == 0:
-                    self._remove_string(other_color_string)
+        new_string = GoString(player, [point], liberties)  # インデントの位置ずれ…許さん
+
+        # 周囲にある連を全てマージする
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+
+        # マージした連の石一つ一つのポイントから，新しくできた連にアクセスできるようにする
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+
+        # 隣接する相手の連から，この石の場所の呼吸点を取り除く
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+
+        # 隣接する相手の連の呼吸点が0になったらその連を取り除く
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
 
     def _remove_string(self, string):
         # 二重ループ，取り除かれる連の各石 -> その石の隣接点
@@ -127,24 +131,24 @@ class Board():
                 if neighbor_string is not string:
                     # ただし，取り除かれる連：自分自身の呼吸点は追加しない
                     neighbor_string.add_liberty(point)
-            
+
             # 石を取り除く．(石のある点のアクセス先の連をなくす)
-            self._grid[point] = None
-
-
+            # self._grid[point] = None
+            del(self._grid[point])
 
     # pointが盤面の範囲内に含まれているか
+
     def is_on_grid(self, point):
-        return  1 <= point.row <= self.num_rows and \
-                1 <= point.col <= self.num_cols
-    
+        return 1 <= point.row <= self.num_rows and \
+            1 <= point.col <= self.num_cols
+
     # 盤面上の点の内容．何もなければNone, 石があればPlayerを返す
     def get(self, point):
         string = self._grid.get(point)
         if string is None:
             return None
         return string.color
-    
+
     # 盤面上のある点を含む連を返す．何もなければNone
     def get_go_string(self, point):
         string = self._grid.get(point)
@@ -155,12 +159,13 @@ class Board():
 
 class GameState():
     """ゲーム状態: 次のプレイヤー，前のゲーム状態，最後の着手"""
+
     def __init__(self, board, next_player, previous, move):
         self.board = board
         self.next_player = next_player
-        self.previous = previous
-        self.move = move
-    
+        self.previous_state = previous
+        self.last_move = move
+
     def apply_move(self, move):
         # 着手が打石ならば，石を置いたときの盤面の変化を計算
         if move.is_play:
@@ -169,10 +174,10 @@ class GameState():
         else:
             # パスや投了ならそのまま
             next_board = self.board
-        
+
         # 新しいゲーム状態を返す
         return GameState(next_board, self.next_player.other, self, move)
-    
+
     @classmethod
     def new_game(cls, board_size):
         """ ゲームの初期化 """
@@ -180,25 +185,25 @@ class GameState():
             board_size = (board_size, board_size)
         board = Board(*board_size)
         return GameState(board, Player.black, None, None)
-    
+
     def is_over(self):
         """ 終局判定 """
         # last_moveが未定義(打石した)なら続行
         if self.last_move is None:
             return False
-        
+
         # どちらかが投了していたら終局
         if self.last_move.is_resign:
             return True
-        
+
         # 連続でパスされていなければ(前の前の手が打石なら，片方がパスしてても)続行
         second_last_move = self.previous_state.last_move
         if second_last_move is None:
             return False
-        
+
         # 連続でパスされていれば終局，そうでなければ続行
         return self.last_move.is_pass and second_last_move.is_pass
-    
+
     def is_move_self_capture(self, player, move):
         """ 自殺手(自らのある連の呼吸点を全て潰す手)かどうか """
         if not move.is_play:
@@ -209,15 +214,15 @@ class GameState():
         next_board.place_stone(player, move.point)
         new_string = next_board.get_go_string(move.point)
         return new_string.num_liberties == 0
-    
+
     @property
     def situation(self):
         return (self.next_player, self.board)
-    
+
     def does_move_violate_ko(self, player, move):
         if not move.is_play:
             return False
-        
+
         # 新しい打石があるたびに
         next_board = copy.deepcopy(self.board)
         next_board.place_stone(player, move.point)
@@ -237,16 +242,14 @@ class GameState():
         # すでに終局しているなら次の着手はない
         if self.is_over():
             return False
-        
+
         # 終局していないなら，パス，もしくは投了を選択することができる
         if move.is_pass or move.is_resign:
             return True
-        
+
         # 打石の場合は，盤面内への打石で，自殺手でも劫でもないことが必要
         return (
             self.board.get(move.point) is None and
             not self.is_move_self_capture(self.next_player, move) and
             not self.does_move_violate_ko(self.next_player, move)
         )
-    
-    
--- dlgo/utils.py
+++ dlgo/utils.py
@@ -7,6 +7,7 @@ STONE_TO_CHAR = {
     gotypes.Player.white: 'o',
 }
 
+
 def print_move(player, move):
     """ 着手の内容を表示．打石は 'black C3' のように表示 """
     if move.is_pass:
@@ -17,14 +18,14 @@ def print_move(player, move):
         move_str = '%s%d' % (COLS[move.point.col - 1], move.point.row)
     print('%s %s' % (player, move_str))
 
+
 def print_board(board):
     """ 盤面全体の状況を表示 """
     for row in range(board.num_rows, 0, -1):
-        bump = " " if row <= 9 else "" # 二桁の行番号になったときのための空白
+        bump = " " if row <= 9 else ""  # 二桁の行番号になったときのための空白
         line = []
         for col in range(1, board.num_cols + 1):
-            stone = board.get(gotypes.Point(row=row, col=col)) # 全ての点の打石状況を確認
+            stone = board.get(gotypes.Point(row=row, col=col))  # 全ての点の打石状況を確認
             line.append(STONE_TO_CHAR[stone])
-        print('%s%d %s' % (bump, row, ''.join(line))) # 空白，行番号，行の石
-    print('    ' + '  '.join(COLS[:board.num_cols]))  # 列記号，ABCD...
-
+        print('%s%d %s' % (bump, row, ''.join(line)))  # 空白，行番号，行の石
+    print('   ' + ''.join(COLS[:board.num_cols]))  # 列記号，ABCD...
